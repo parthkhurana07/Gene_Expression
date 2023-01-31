642859215ec5ceccaec77c06ec1ca027c8dc673d
parthkhurana07 <khuranaparth07@gmail.com>
2023-01-30 23:20:06 -0500

    FIles

--- scripts/genexp.R
+++ scripts/genexp.R
@@ -85,4 +85,38 @@ data.long %>%
   ggplot(.,aes(x = Samples, y = Gene, fill = FPKM)) +
   geom_tile() +
   scale_fill_gradient(low = 'white', high = 'red')
-  
+  #barPlot
+b<-data.long %>%
+  filter(Gene == 'aaeA') %>%
+  ggplot(.,aes(x = Samples, y = FPKM, fill = Time)) +
+  geom_col() +
+  scale_fill_brewer(palette = "Dark2")
+
+#densityPlot
+d<-data.long %>%
+  filter(Gene == "aaeA") %>%
+  ggplot(.,aes(FPKM, fill = Time)) +
+  geom_density(alpha = 0.3) +
+  scale_fill_brewer(palette = "Dark2")
+
+#scatterPlot
+s<-data.long %>%
+  filter(Gene == "aaeA" | Gene == "abgA") %>%
+  spread(key = Gene, value = FPKM) %>%
+  ggplot(.,aes(x = aaeA, y = abgA, color = Time)) +
+  geom_point() +
+  geom_smooth(method = 'lm', se = F)
+
+#heatmap
+genes.of.interest<-c("aaeA", "abgA", "aaeB", "abgB")
+h<-data.long %>%
+  filter(Gene %in% genes.of.interest) %>%
+  ggplot(.,aes(x = Samples, y = Gene, fill = FPKM)) +
+  geom_tile() +
+  scale_fill_gradient(low = 'white', high = 'red')
+
+#saving the plots
+ggsave(b, filename = 'wullfen_barplot.pdf', width = 10,height = 8)
+ggsave(d, filename = 'wullfen_densityplot.pdf', width = 10,height = 8)
+ggsave(s, filename = 'wullfen_scatterplot.pdf', width = 10,height = 8)
+ggsave(h, filename = 'wullfen_heatmapt.pdf', width = 10,height = 8)
